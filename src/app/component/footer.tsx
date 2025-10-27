73527b614b113abed64fc32d1b101416994abeb9
kumagai <akihirok1341@icloud.com>
2025-10-28 00:07:39 +0900

    footerの作成  fix  /home, /dashboardを正しい表記に修正。 ホバーを残しつつ、ページ遷移後のカラーを固定

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -5,27 +5,37 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
+import { usePathname } from 'next/navigation'
 // MUIからアイコンを拾ってくる
-import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
-import HomeFilledIcon from '@mui/icons-material/HomeFilled';
+import InsightsTwoToneIcon from '@mui/icons-material/InsightsTwoTone'
+import HomeFilledIcon from '@mui/icons-material/HomeFilled'
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 
 const Footer = () => {
+    const pathname = usePathname()
+
     return (
         <footer className="fixed bottom-0 left-0 w-full bg-gray-300 py-3 flex justify-evenly items-center">
-            <Link href="/home">
-                <div className="hover:bg-white px-5 py-3 rounded-full transition-colors duration-200">
-                    <HomeFilledIcon className='text-black' />
+            <Link href="/top">
+                <div className={`px-5 py-3 rounded-full transition-colors duration-200
+                ${pathname === '/top' ? 'bg-[#a9a9a9]' : 'bg-transparent hover:bg-white'}`}
+                >
+                    <HomeFilledIcon className="!text-black transition-colors duration-200" />
                 </div>
-            </Link>
-            <Link href="/dashboard">
-                <div className="hover:bg-white px-5 py-3 rounded-full transition-colors duration-200">
-                    <SignalCellularAltIcon className="text-black" />
-                </div>
+
             </Link>
             <Link href="/spot">
-                <div className="hover:bg-white px-5 py-3 rounded-full transition-colors duration-200">
-                    <BookmarkIcon className="text-black" />
+                <div className={`px-5 py-3 rounded-full transition-colors duration-200
+                ${pathname === '/spot' ? 'bg-[#a9a9a9]' : 'bg-transparent hover:bg-white'}`}
+                >
+                    <BookmarkIcon className="!text-black transition-colors duration-200" />
+                </div>
+            </Link>
+            <Link href="/user">
+                <div className={`px-5 py-3 rounded-full transition-colors duration-200
+                ${pathname === '/user' ? 'bg-[#a9a9a9]' : 'bg-transparent hover:bg-white'}`}
+                >
+                    <InsightsTwoToneIcon className="!text-black transition-colors duration-200" />
                 </div>
             </Link>
         </footer>
