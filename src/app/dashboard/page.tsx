911b2ca7203cf94f44acb41eb3a96f042f4279f0
yuge0915 <yuge.a.24kdgh@gmail.com>
2025-11-10 14:29:33 +0900

    dashboard UI完成 (#22)
    Co-authored-by: Takuya0202 <shimozono.t.24kdgn@gmail.com>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
 "use client";
+import { Card } from "../components/dashboard/card";
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
+import InsightsIcon from '@mui/icons-material/Insights';
+import Footer from "../component/footer";
 import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
 import Logout from "../feature/auth/logout";
@@ -18,11 +22,14 @@ export default function Dashboard() {
     getUser();
   });
   return (
-    <div>
-      <p>{error}</p>
-      <p>{name}</p>
-      <h1>Dashboard</h1>
+        <div className="h-full  px-2 ">
+            <div className=" flex flex-col items-center justify-between gap-11 mt-[62px]">
+                <Card name="今日の走行距離" className="" distance={10.7} divClassName="" icon={<TwoWheelerIcon  className='!text-[95px] mr-7 '/>} />
+                <Card name="一週間の走行距離" className="" distance={108.7} icon={<InsightsIcon  className='!text-[95px] mr-7 '/>} />
+                <Card name="今までの走行距離" className="" distance={108.7} icon={<InsightsIcon  className='!text-[95px] mr-7 '/>} />
+            </div>
       <Logout />
-    </div>
+            <Footer />
+        </div>
   );
 }
