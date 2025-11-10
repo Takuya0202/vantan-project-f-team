b0a12c6db749bdef4acba1bba8aa8975606fa582
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-11-10 17:37:31 +0900

    ダッシュボードのエラーfix (#24)

--- src/app/components/dashboard/card.tsx
+++ src/app/components/dashboard/card.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
 type CardProps = {
-    name: string;
-    className?: string;
-    distance: number;
-    divClassName?: string;
-    icon: React.ReactNode;
-}
+  name: string;
+  className?: string;
+  distance: number;
+  divClassName?: string;
+  icon: React.ReactNode;
+};
 
 export function Card(props: CardProps) {
-    const {name, className, distance, divClassName, icon} = props
-    return(
-        <div className={`bg-gray-300 w-[339px] h-[117px] rounded-[5px] flex justify-between items-center ${divClassName}`}>
-            <div className='ml-[20px]'>
-                <div className='mb-[24px]'>
-                    <p className='text-[14px]'>{name}</p>
-                </div>
-                <div className={`${className}`}>
-                    <p className='text-[24px] font-sans font-bold'>{distance}km</p>
-                </div>
-            </div>
-            <div>
-                {icon}
-            </div>
+  const { name, className, distance, divClassName, icon } = props;
+  return (
+    <div
+      className={`bg-gray-300 w-[339px] h-[117px] rounded-[5px] flex justify-between items-center ${divClassName}`}
+    >
+      <div className="ml-[20px]">
+        <div className="mb-[24px]">
+          <p className="text-[14px]">{name}</p>
         </div>
-    )
+        <div className={`${className}`}>
+          <p className="text-[24px] font-sans font-bold">{distance}km</p>
+        </div>
+      </div>
+      <div>{icon}</div>
+    </div>
+  );
 }
