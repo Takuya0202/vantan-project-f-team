23d229c9c62d91b6961ab3136e4fbf3978430f1d
iwaki0710 <iwaki.s.24kdgn@gmail.com>
2025-11-14 15:30:08 +0900

    機能/スポットUI (#25)
    * 11/08
    
    * 11/14 SpotUI
    
    * 11/14 SpotUI更新
    
    * fix
    
    * add

---
+++ src/app/spotui/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SpotRanking from '../component/spot'; 
+
+export default function Spotui() {
+
+    return(
+        <>
+            <div>
+                <SpotRanking />
+            </div>
+        </>
+    );
+}
